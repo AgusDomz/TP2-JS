df849e7c315734198b54899d0aa9de5189c76d05
agusdomz <agusdominguez95@gmail.com>
2023-05-24 04:14:13 -0300

    problemas casi terminados

--- js.js
+++ js.js
@@ -11,7 +11,7 @@
 	}
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
@@ -58,7 +58,7 @@ Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje
 	}
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
@@ -66,31 +66,52 @@ Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje
 pedir cadenas de texto
 cancelar
 mostrar cadenas de texto concatenadas con -
+let cadenas = [];
 
+	while (true) {
+		let input = prompt("Introduce un texto.");
 
-	let cadenas = "";
-
-	while (true){
-		let cadena = prompt("Dime una frase!");
-
-		if (cadena === null) {
+		if (input === null) {
 			break;
 		}
-	} cadenas += cadena + "-";
 
-	cadenas = cadenas.slice(0, -1);
+		cadenas.push(input);
 
-		console.log(cadenas)
+	}
 
-*/
-//--------------------------------------------------------------------------------------------------------------------------------
+	let resultado = cadenas.join("-");
+	
+	console.log(resultado);
+
+*/	
+//-----------------------------------------------------------------------------------
 /*
 
 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
 
+	let suma = 0;
+
+	while (true) {
+		let input = prompt("Dame un numero");
+
+		if (input === null) {
+			break;
+		}
+
+		let numero = parseFloat(input);
+
+		if (isNaN(numero)) {
+			alert("No es un numero valido");
+		}
+
+		suma += numero;
+	}
+
+	alert("La suma total es: " + suma);
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 5- Realizar una página con un script que calcule el valor de la letra de un número de DNI (Documento nacional de identidad).
@@ -103,8 +124,30 @@ Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  
 Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
 Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 
+
+
+		while (true) {
+		let dni = prompt("Introduce un numero de DNI: ");
+
+		if (dni === null) {
+			break;
+		}
+
+		if (isNaN(dni)) {
+			alert("No es un numero valido")
+		}
+		
+		let numero = parseInt(dni);
+		let letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+		let i = numero % 23;
+		let letra = letras.charAt(i);
+
+		alert("La letra de DNI " + numero + " es: " + letra);
+	}
+
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
@@ -123,7 +166,7 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 	}
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
@@ -147,7 +190,7 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 	}
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 8- Crea script para generar pirámide siguiente con los números del 1 al número que indique el usuario (no mayor de 50) 
@@ -159,9 +202,27 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 12345
 123456
 ……
+let numero = parseInt(prompt("Ingresa un numero."));
 
-*/
-//--------------------------------------------------------------------------------------------------------------------------------
+	let numero = parseInt(prompt("Introduce un numero hasta 50: "));
+
+	if (isNaN(numero) || numero <= 0 || numero > 50) {
+		alert("El número ingresado no es válido.");
+	} else {
+	let piramide = "";
+
+	for (let i = 1; i <= numero; i++) {
+		for (let j = 1; j <= i; j++) {
+		piramide += j + " ";
+	}
+		piramide += "\n";
+	}
+
+		console.log(piramide);
+	}
+
+*/	
+//-----------------------------------------------------------------------------------
 /*
 
 9- Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
@@ -178,54 +239,126 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 9 (Múltiplo de 9)
 10
 
+
+
+	for (let i = 1; i <= 500 ; i++) {
+		let mensaje = i
+
+		if (i % 4 === 0) {
+			mensaje += " (Multiplo de 4)";
+		} else if (i % 9 === 0){
+			mensaje += " (Multiplo de 9)"; 
+		}
+
+		console.log(mensaje);
+
+		if (i % 5 === 0) {
+			console.log("-------------------");
+		}
+	}
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
+	let filas = parseInt(prompt("Introduce el número de filas:"));
+	let columnas = parseInt(prompt("Introduce el número de columnas:"));
+	let numero = filas * columnas;
+
+	document.write("<table>");
+
+	for (let i = 0; i < filas; i++) {
+		document.write("<tr>");
+
+	for (let j = 0; j < columnas; j++) {
+		document.write("<td>" + numero + "</td>");
+		numero--;
+	}
+
+		document.write("</tr>");
+	}
+
+	document.write("</table>");
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
 
+
+
+	let edades = [];
+	let nombres = [];
+
+	for (let i = 0; i < 3; i++) {
+		let edad = parseInt(prompt("Introduce una edad"));
+		let nombre = prompt("introduce un nombre");
+
+		edades.push(edad);
+		nombres.push(nombre);
+	}
+
+	let mayor = Math.max(...edades);
+	let indiceMayor = edades.indexOf(mayor);
+	let nombreMayor = nombres[indiceMayor];
+
+	console.log(nombreMayor + " es el mayor");
+
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 12- Realiza un script que genere un número aleatorio entre 1 y 99
 
+
+	let numRandom = Math.floor(Math.random() * 100);
+
+	console.log(numRandom);
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
 
+
+
+	let texto = prompt("Dale poder a tus palabras!");
+
+	console.log(texto.toUpperCase());
+
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 15- Realiza un script que cuente el número de vocales que tiene un texto.
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
 
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 /*
 
 17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 
+let texto = prompt("introduce un texto.");
+	console.log("La primera vocal esta en la posicion: " + texto.indexOf("a", "e", "i", "o", "u"));
 */
-//--------------------------------------------------------------------------------------------------------------------------------
+
+	
+//-----------------------------------------------------------------------------------
 
